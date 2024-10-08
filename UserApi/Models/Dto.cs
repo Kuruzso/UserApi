d86c2a68c94e1234cf12b48578d70d7be0be12ef
Kuruzso <Vellera@kkszki.hu>
2024-10-08 13:37:10 +0200

    post and put ready

---
+++ UserApi/Models/Dto.cs
@@ -0,0 +1,11 @@
+﻿namespace UserApi.Models
+{
+    public class Dto
+    {
+        public record CreateUserDto(string Name,int Age,bool License);
+
+        public record UpdateUserDto(string Name, int Age, bool License);
+
+
+    }
+}
--- UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
+++ UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089a999a2384ff2c009f2c6a4301f51186acb081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea36bca04bf9c783fa3c3c6b83d4e02a3b7c4d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
