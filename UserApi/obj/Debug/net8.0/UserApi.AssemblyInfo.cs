089a999a2384ff2c009f2c6a4301f51186acb081
Kuruzso <Vellera@kkszki.hu>
2024-10-08 12:17:39 +0200

    Database export file is added to solution folder

--- UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
+++ UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc132c7ce17a3c239b8470332a49f159e9e69624")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
